4f7dac0e10e8a2895bb99bda841cb130bddb972d
henghenghengforever <henghenghengforever@gmail.com>
2018-09-05 19:13:02 +0800

    update
    rename the filename and add the floder of Lumerical Scripts

---
+++ Lumerical_scp/add_hole_structure.m
@@ -0,0 +1,77 @@
+addstructuregroup;
+    set("name","hole");
+addcircle;
+    set("name","hole_1");
+    set("x",0e-6);
+    set("y",0e-6);
+    set("radius",0.7e-6);
+    set("z",0.02e-6);
+    set("z span",0.04e-6);
+    set("material","etch");
+    addtogroup("hole"); 
+addcircle;
+    set("name","hole_2");
+    set("x",2.511e-6);
+    set("y",1.45e-6);
+    set("radius",0.7e-6);
+    set("z",0.02e-6);
+    set("z span",0.04e-6);
+    set("material","etch");
+    addtogroup("hole"); 
+addcircle;
+    set("name","hole_3");
+    set("x",0e-6);
+    set("y",2.9e-6);
+    set("radius",0.7e-6);
+    set("z",0.02e-6);
+    set("z span",0.04e-6);
+    set("material","etch");
+    addtogroup("hole"); 
+addcircle;
+    set("name","hole_4");
+    set("x",-2.511e-6);
+    set("y",1.45e-6);
+    set("radius",0.7e-6);
+    set("z",0.02e-6);
+    set("z span",0.04e-6);
+    set("material","etch");
+    addtogroup("hole");
+addcircle;
+    set("name","hole_5");
+    set("x",-2.511e-6);
+    set("y",-1.45e-6);
+    set("radius",0.7e-6);
+    set("z",0.02e-6);
+    set("z span",0.04e-6);
+    set("material","etch");
+    addtogroup("hole"); 
+addcircle;
+    set("name","hole_6");
+    set("x",0e-6);
+    set("y",-2.9e-6);
+    set("radius",0.7e-6);
+    set("z",0.02e-6);
+    set("z span",0.04e-6);
+    set("material","etch");
+    addtogroup("hole"); 
+ addcircle;
+    set("name","hole_7");
+    set("x",2.511e-6);
+    set("y",-1.45e-6);
+    set("radius",0.7e-6);
+    set("z",0.02e-6);
+    set("z span",0.04e-6);
+    set("material","etch");
+    addtogroup("hole"); 
+
+
+
+
+
+
+
+
+
+
+
+
